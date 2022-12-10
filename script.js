c4723526d2253801105b6051254eea4e1c5fd899
tsv-stacks-linux <tsvirdi@yahoo.co.uk>
2022-12-10 00:01:52 +0000

    added modal name form

---
+++ script.js
@@ -0,0 +1,20 @@
+let inputText = document.getElementById("input-name");
+let greetText = document.getElementById("greeting");
+
+const nameGen = () => {
+  let testText = inputText.value.trim();
+  if (testText === "") {
+    return alert(
+      "Please type your name to get started with your very own virtual Baby Groot Pet!"
+    );
+  } else {
+    testText = titleCase(testText);
+    greetText.innerText = `Hello ${testText}\n I am Groot!`;
+    document.getElementById("modal-add-name").classList.add("fade-out");
+    return console.log("name recieved");
+  }
+};
+
+function titleCase(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
