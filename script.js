8ff533c30a40fc3051e878dbb4373971200f001b
tsv-stacks-linux <tsvirdi@yahoo.co.uk>
2022-12-11 21:48:45 +0000

    added sound

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const nameGen = () => {
       "Please type your name to get started with your very own virtual Baby Groot Pet!"
     );
   } else {
+    play();
     testText = titleCase(testText);
     greetText.innerText = `Hello ${testText}\n I am Groot!`;
     document.getElementById("modal-add-name").classList.add("fade-out");
@@ -18,3 +19,8 @@ const nameGen = () => {
 function titleCase(string) {
   return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
+
+const play = () => {
+  let audio = document.getElementById("audio");
+  audio.play();
+};
