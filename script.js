62f60f1aedbdd710c200ee18b17514f995efb6dc
tsv-stacks-linux <tsvirdi@yahoo.co.uk>
2022-12-19 21:57:02 +0000

    cleaning code

--- script.js
+++ script.js
@@ -143,8 +143,6 @@ const nameGen = () => {
   }
 };
 
-const gameStart = () => {};
-
 //converts name to Case
 const titleCase = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
